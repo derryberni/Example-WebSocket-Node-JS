d02e02d50e73b6c7f8fc1c6255996dfb0ebcd78f
derry <makara@192.168.1.4>
2021-08-05 16:00:44 +0700

    add broadcast to all client

--- index.js
+++ index.js
@@ -12,13 +12,28 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/doraemon', (req, res) => {
+  wss.send('doraemon');
+  res.json({ 'status': 200 });
+});
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    wss.broadcast(message);
   });
 
-  ws.send('something');
+  ws.send('Selamat Datang');
 });
+
+wss.broadcast = function broadcast(msg) {
+   var newMsg = msg.toString();
+   console.log(newMsg);
+   wss.clients.forEach(function each(client) {
+       console.log(client);
+       client.send(newMsg);
+    });
+};
 
 //expose port 3000 server
 server.listen(3000, function(){
